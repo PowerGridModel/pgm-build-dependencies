732ca790812f1a2a6db00b1faa04556202da4384
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2025-05-28 12:37:08 +0200

    start package
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

---
+++ src/pgm_build_dependencies/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from pathlib import Path
+
+from importlib_resources import files
+
+
+def get_include() -> Path:
+    return files("pgm_build_dependencies.include")
