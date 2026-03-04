97d9b851aa0fca0ed254362a76077af6d4980d8e
Santiago Figueroa Manrique <figueroa1395@gmail.com>
2026-03-04 17:43:21 +0100

    output complete path, not common
    Signed-off-by: Santiago Figueroa Manrique <figueroa1395@gmail.com>

--- src/pgm_build_dependencies/cmake_prefix_path.py
+++ src/pgm_build_dependencies/cmake_prefix_path.py
@@ -6,17 +6,12 @@ from importlib.metadata import entry_points
 import os
 
 def cmake_prefix_path():
-    """Output a single CMAKE_PREFIX_PATH usable for CMake (common parent of all cmake.root packages).
-    """
+    """Output CMAKE_PREFIX_PATH for all pgm build dependencies"""
     cmake_roots = entry_points(group="cmake.root")
-
+    
     paths = []
     for ep in cmake_roots:
         loaded = ep.load()
         paths.append(loaded.__path__[0])
-
-    if paths:
-        common_path = os.path.commonpath(paths)
-        print(common_path)
-    else:
-        print("")
+    
+    print(os.pathsep.join(paths))
